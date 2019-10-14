6c055786afc44513aed4da6e693dfbc6fcc02c15
Ricardo do Canto <endurancecode@gmail.com>
2019-10-14 13:22:36 +0100

    Chain methods

--- spring-mvc-introduction/src/main/java/com/pluralsight/controller/MinutesController.java
+++ spring-mvc-introduction/src/main/java/com/pluralsight/controller/MinutesController.java
@@ -14,6 +14,19 @@ public class MinutesController {
 
         System.out.println("Exercise: " + exercise.getMinutes());
 
+        /*
+         * "redirect:" closes the first request and creates a new request
+         * "forward:" can be used instead of "redirect:" but i doesn't closes
+         * the first request, it just forward the same request
+         */
+        return "redirect:addMoreMinutes.html";
+    }
+
+    @RequestMapping(value = "/addMoreMinutes")
+    public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise) {
+
+        System.out.println("Exercising: " + exercise.getMinutes());
+
         return "addMinutes";
     }
 }
