076fab73cd17a2d4211143a43398e09ea14b1809
Ricardo do Canto <endurancecode@gmail.com>
2019-10-24 14:51:26 +0100

    Update getRides()

--- spring-jdbc/src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ spring-jdbc/src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -1,10 +1,12 @@
 package com.pluralsight.repository;
 
-import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
@@ -25,11 +27,21 @@ public class RideRepositoryImpl implements RideRepository {
 
     @Override
     public List<Ride> getRides() {
-        Ride ride = new Ride();
-        ride.setName("Corner Canyon");
-        ride.setDuration(120);
-        List<Ride> rides = new ArrayList<>();
-        rides.add(ride);
+
+        List<Ride> rides = jdbcTemplate.query("select * from ride", new RowMapper<Ride>() {//NOSONAR
+
+            @Override
+            public Ride mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+
+                Ride ride = new Ride();
+                ride.setId(resultSet.getInt("id"));
+                ride.setName(resultSet.getNString("name"));
+                ride.setDuration(resultSet.getInt("duration"));
+
+                return ride;
+            }
+        });
+
         return rides;
     }
 }
