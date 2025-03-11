e11252c355ef34b6f31978c269f44c8e1c3bcf42
Ricardo do Canto <endurancecode@gmail.com>
2025-03-11 21:10:02 +0000

    MTS: Render Thymeleaf templates with Spring MVC

---
+++ sfg-thymeleaf-course/src/main/java/guru/springframework/contollers/IndexController.java
@@ -0,0 +1,13 @@
+package guru.springframework.contollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+  @RequestMapping("/")
+  public String getIndexPage() {
+    return "index";
+  }
+}
---
+++ sfg-thymeleaf-course/src/main/java/guru/springframework/contollers/ProductController.java
@@ -0,0 +1,13 @@
+package guru.springframework.contollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductController {
+
+  @RequestMapping("/product")
+  public String getProduct() {
+    return "product";
+  }
+}
