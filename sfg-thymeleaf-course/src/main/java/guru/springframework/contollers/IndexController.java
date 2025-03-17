4b3365e7fba85fff1eeb495765b3555e4987ba4a
Ricardo do Canto <endurancecode@gmail.com>
2025-03-17 22:27:45 +0000

    MTS: List objects with Thymeleaf and Spring MVC

--- sfg-thymeleaf-course/src/main/java/guru/springframework/contollers/IndexController.java
+++ sfg-thymeleaf-course/src/main/java/guru/springframework/contollers/IndexController.java
@@ -1,13 +1,26 @@
 package guru.springframework.contollers;
 
+import guru.springframework.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+  private final ProductService productService;
+
+  @Autowired
+  public IndexController(ProductService productService) {
+    this.productService = productService;
+  }
+
   @RequestMapping("/")
-  public String getIndexPage() {
+  public String getIndexPage(Model model) {
+
+    model.addAttribute("products", productService.getProducts());
+
     return "index";
   }
 }
